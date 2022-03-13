f5affe886338dbad14e80259cf1f751490e4bd2a
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-03-13 20:37:46 +0300

    39-end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+} from "react-router-dom";
+import AddPlace from "./pages/AddPlace";
+import Users from "./pages/Users";
 
 function App() {
-  return <h1>Let's start!</h1>;
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<Users />
+				</Route>
+				<Route path="/places/add" exact>
+					<AddPlace />
+				</Route>
+				<Redirect to="/" />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
