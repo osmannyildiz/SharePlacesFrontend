d63df406ff5ffa73f8db5a87e3b0a7c8a87fd2fa
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-03-18 21:32:04 +0300

    47-end

--- src/components/nav/SideDrawer.js
+++ src/components/nav/SideDrawer.js
@@ -1,13 +1,18 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import NavLinks from "./NavLinks";
 import "./SideDrawer.css";
 
 export default function SideDrawer() {
-	return (
+	const content = (
 		<aside className="side-drawer">
 			<nav className="side-drawer-nav">
 				<NavLinks />
 			</nav>
 		</aside>
+	);
+	return ReactDOM.createPortal(
+		content,
+		document.getElementById("side-drawer-portal")
 	);
 }
