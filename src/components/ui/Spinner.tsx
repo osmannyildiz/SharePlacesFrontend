f90af15fc7ddb54df6d39003cb98b7ccaf1b29ad
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-10-12 15:45:27 +0300

    Add color prop to Spinner

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -3,11 +3,15 @@ import "./Spinner.scss";
 
 interface Props {
 	asOverlay?: boolean;
+	color?: string;
 }
 
 export default function Spinner(props: Props) {
 	return (
-		<div className={cn("spinner", props.asOverlay && "spinner--as-overlay")}>
+		<div
+			className={cn("spinner", props.asOverlay && "spinner--as-overlay")}
+			style={{ ["--color" as any]: props.color || undefined }}
+		>
 			<div className="spinner__dual-ring"></div>
 		</div>
 	);
